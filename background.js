6452c8c113b43cc702d01452530edea22176e27a
zhuml1 <zhuml1@ifeng.com>
2024-06-13 19:19:30 +0800

    feat: Add Shortcut

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
     id: 'copyPageInfo',
-    title: 'Copy Page URL and Title',
+    title: 'Copy Title URL (Ctrl+Shift+Y)',
     contexts: ['page', 'selection']
   });
 });
@@ -12,6 +12,17 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
       target: { tabId: tab.id },
       function: copyPageInfo
     });
+  }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'copy-title-url') {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: copyPageInfo
+    });
+  });
   }
 });
 
