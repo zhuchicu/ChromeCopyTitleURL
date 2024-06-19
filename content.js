465f1d31706e04f3cd377f5e9415378944311701
zhuml <zhuml@win>
2024-06-20 01:00:26 +0800

    feat: 添加选中文本弹出图标，且可复制文本

--- content.js
+++ content.js
@@ -1,38 +1,37 @@
 // 创建一个浮动按钮
-const button = document.createElement('button');
-button.innerText = 'Copy';
-button.style.position = 'absolute';
-button.style.display = 'none';
-button.style.zIndex = 1000;
-button.style.padding = '5px 10px';
-button.style.border = '1px solid #ccc';
-button.style.backgroundColor = '#fff';
-button.style.cursor = 'pointer';
-document.body.appendChild(button);
+const icon = document.createElement('img');
+icon.src = chrome.runtime.getURL('images/logo.jpg');
+icon.id = 'selection-icon'
+icon.alt = 'Copy to clipboard';
+icon.style.display = 'none';
+document.body.appendChild(icon);
+
+
 
 // 监听鼠标释放事件
 document.addEventListener('mouseup', function(event) {
   console.log('监听鼠标释放事件')
   const selectedText = window.getSelection().toString().trim();
-  if (selectedText) {
-    const rect = window.getSelection().getRangeAt(0).getBoundingClientRect();
-    button.style.top = `${rect.top + window.scrollY + rect.height}px`;
-    button.style.left = `${rect.left + window.scrollX}px`;
-    button.style.display = 'block';
+  if (selectedText.length > 0) {
+    const x = event.pageX;
+    const y = event.pageY;
+    icon.style.left = `${x}px`;
+    icon.style.top = `${y}px`;
+    icon.style.display = 'block';
   } else {
-    button.style.display = 'none';
+    icon.style.display = 'none';
   }
 });
 
 // 点击按钮时复制文本
-button.addEventListener('click', function() {
+icon.addEventListener('click', function() {
   console.log('点击按钮时复制文本')
   const selectedText = window.getSelection().toString().trim();
   if (selectedText) {
     const text = pangu.spacing(selectedText)
     navigator.clipboard.writeText(text).then(() => {
       // alert('Text copied to clipboard!');
-      button.style.display = 'none';
+      icon.style.display = 'none';
     });
   }
 });
